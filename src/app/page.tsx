aed003f9da934f2c16aa737065f0bd479083d7ac
demiro <demiroezkan205@gmail.com>
2024-03-27 03:38:11 +0100

    Favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 space-x-2">
           <code className="font-mono font-bold">
-            <Link href="https://github.com/blefnk/relivator-docker-frontend">
+            <Link href="https://github.com/demiroo/relivator-docker-frontend">
               relivator-docker-frontend
             </Link>
           </code>
           <code className="font-mono underline">
-            <Link href="https://github.com/blefnk/relivator-docker-backend">
+            <Link href="https://github.com/demiro/relivator-docker-backend">
               relivator-docker-backend
             </Link>
           </code>
